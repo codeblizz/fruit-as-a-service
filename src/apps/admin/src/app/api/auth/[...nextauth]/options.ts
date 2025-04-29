b14c6c8df87db7148cc9530f704c8361e99a6a7e
codeblizz <codeblizzen@gmail.com>
2025-04-29 17:50:20 +0400

    feat(layout): add layouts and global css for web and admin dashboard views

---
+++ src/apps/admin/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,38 @@
+import type { NextAuthOptions } from "next-auth";
+import credentialsProvider from "next-auth/providers/credentials";
+import {
+  CredentialType,
+} from "@/packages/types/auth.type";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    credentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      type: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<string, string> | undefined
+      ) {
+        if (!credentials) return null;
+        const { email, password } = credentials as CredentialType;
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+    error: "/error",
+  },
+  jwt: {
+    maxAge: 3600,
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 3600,
+  },
+  callbacks: {},
+};

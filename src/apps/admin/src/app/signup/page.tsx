9020f029d1af9e550ff3d3fd223cb1bf8fb04557
codeblizz <codeblizzen@gmail.com>
2025-04-29 18:15:49 +0400

    feat(auth): add routes and pages for web and admin dashboard views

--- src/apps/admin/src/app/signup/page.tsx
+++ src/apps/admin/src/app/signup/page.tsx
@@ -1,9 +1,9 @@
 import Fragment from "@/packages/ui/atoms/fragment";
 import AuthForm from "@/packages/ui/molecules/authForm";
 
-export default function Home() {
+export default function SignUp() {
   return (
-    <Fragment className="min-h-screen w-full bg-[url('/images/fruit-platter-003.webp')] bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center">
+    <Fragment className="min-h-screen bg-[url('/images/fruit-platter-001.webp')] bg-no-repeat bg-cover bg-center w-full flex flex-col justify-center items-center">
       {/* <Fragment className="flex items-center justify-center w-full min-h-screen backdrop-blur-sm"> */}
         <AuthForm className="w-[60%] sm:w-[50%] md:w-[40%] h-[50%] min-w-max" />
       {/* </Fragment> */}

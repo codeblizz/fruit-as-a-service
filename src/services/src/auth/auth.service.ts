9c21f7acd860c3bfaf0a5422450f768dfa823184
codeblizz <codeblizzen@gmail.com>
2025-05-04 23:50:50 +0400

    feat(service): add service layer to handle auth logic

---
+++ src/services/src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import AuthRepository from "@/repository/src/auth.repository";
+import RepositoryFactory from "@/repository/src/factory.repository";
+import {
+  AuthInterface,
+  SignInType,
+  SignUpType,
+} from "@/packages/types/src/auth.type";
+
+export function AuthService<T>(urlResource: string) {
+  
+  let api: AuthInterface;
+  const authRepository = AuthRepository<T>(urlResource);
+  api = RepositoryFactory().get(urlResource) as unknown as AuthInterface;
+  
+  return Object.assign({}, authRepository, {
+    signIn: async (credentials: SignInType) => {
+      return await api.signIn(credentials);
+    },
+    signUp: async (payload: SignUpType) => {
+      return await api.signUp(payload);
+    },
+    getRefreshToken: async (payload: any) => {
+      return await api.getRefreshToken(payload);
+    }
+  });
+}

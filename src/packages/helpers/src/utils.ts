b9018e54b19cddda0fe702ff5d87f360745a5cdc
codeblizz <codeblizzen@gmail.com>
2025-05-05 00:13:45 +0400

    feat(utils): add utility functions to handling token expiration, refresh access token

--- src/packages/helpers/src/utils.ts
+++ src/packages/helpers/src/utils.ts
@@ -1,21 +1,14 @@
 import { ErrorType } from "@/packages/store/src/errorSlice";
-import { CustomError } from "@/packages/types/src/utils.type";
+import { AuthToken, RefreshToken } from "@/packages/types/src/auth.type";
+import { URLResource, UnionFromURLResource, CustomError } from "@/packages/types/src/utils.type";
+import { AxiosResponse } from "axios";
+import CONSTANT from "./constants";
+import { JWT } from "next-auth/jwt";
+import { AuthService } from "@/services/src/auth/auth.service";
 
 const utils = {
-  defaultUser: {
-    email: "",
-    fullName: "",
-    username: "",
-    profilePic: "",
-    agreement: false,
-    roles: {
-      user: 0,
-      admin: 1,
-    },
-    permissions: {
-      user: 0,
-      admin: 1,
-    },
+  isUnionFromURLResource: function(value: string): value is UnionFromURLResource {
+    return Object.values(URLResource).includes(value as UnionFromURLResource)
   },
   customError: function (message: string, code?: number) {
     const error = new Error(message) as CustomError;
@@ -54,6 +47,47 @@ const utils = {
       statusCode,
       status: false,
     };
+  },
+  checkIfTokenExpired: function (expiresAt: number) {
+    const dateNow = Math.floor(Date.now() / 1000);
+    return dateNow > expiresAt;
+  },
+  getOAuthUser: function (token: JWT) {
+    return {
+      ...CONSTANT.defaultUser,
+      type: token.type,
+      email: token.email,
+      fullName: token.name,
+      profilePic: token.image,
+      provider: token.provider,
+      lastName: token.family_name,
+      firstName: token.given_name,
+      username: token.given_name + "_" + token.id,
+      roles: { ...CONSTANT.defaultUser.roles, user: 1 },
+    };
+  },
+  refreshAccessToken: async function (
+    refreshToken: RefreshToken
+  ): Promise<AuthToken> {
+    let token = {} as AuthToken;
+
+    try {
+      const { data } = await AuthService("refreshToken").getRefreshToken(
+        refreshToken
+       ) as unknown as AxiosResponse;
+      token = {
+        error: null,
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      };
+      return token;
+    } catch (error: unknown) {
+      console.error("refreshAccessToken error", error);
+      throw {
+        ...token,
+        error: "RefreshAccessTokenError",
+      };
+    }
   },
 };
 

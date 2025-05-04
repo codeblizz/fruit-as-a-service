8366a60f65589594d042eb15076040b428d62696
codeblizz <codeblizzen@gmail.com>
2025-05-05 00:02:42 +0400

    refactor(schema): remove repetitive schema properties

--- src/packages/helpers/src/validations/auth.validate.ts
+++ src/packages/helpers/src/validations/auth.validate.ts
@@ -18,24 +18,9 @@ export const LoginSchema = z.object({
     ),
 });
 
-export const RegisterSchema = z.object({
-  email: z
-    .string()
-    .email("Field not an email format")
-    .min(5)
-    .nonempty("Email field is empty ")
-    .trim(),
-  password: z
-    .string()
-    .min(5)
-    .max(30)
-    .nonempty("Password field is empty")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$/,
-      "At least a capital, small letter, & special character"
-    ),
+export const RegisterSchema = LoginSchema.extend({
   acceptTerms: z
     .boolean()
     .default(false)
-    .refine((term) => (term === false ? "Field is required" : "")),
+    .refine((term) => term === true, { message: "Field is required" }),
 });
